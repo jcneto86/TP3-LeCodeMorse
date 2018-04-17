9e3add700b1556d0f6842281bd8da9b898fcd117
João Carlos Fernandes Neto <wysrar@gmail.com>
2018-04-17 14:19:05 -0400

    v 0.9

--- app/src/main/java/com/jcneto/tp3_lecodemorse/MainActivity.java
+++ app/src/main/java/com/jcneto/tp3_lecodemorse/MainActivity.java
@@ -16,6 +16,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     private String morse = "";
+    private String alpha = "";
     private final TraducteurMorse traducteurMorse = new TraducteurMorseConcrete();
     private AlertDialog alert;
     @Override
@@ -24,6 +25,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         final EditText editTextMorse = (EditText) findViewById(R.id.editTextMorse);
         final EditText editTextAlpha = (EditText) findViewById(R.id.editTextAlpha);
+        // Pour afficher une msg
+        /**
+         *
+         */
         final AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Attention !");
         builder.setMessage(
@@ -65,6 +70,7 @@ public class MainActivity extends AppCompatActivity {
                 textViewSorti.setText(traducteurMorse.toMorse(editable.toString()));
                 TextView textViewEntre = (TextView) findViewById(R.id.textEntre);
                 textViewEntre.setText(traducteurMorse.nettoyerAlpha(editable.toString()));
+                morse = traducteurMorse.toMorse(editable.toString());
             }
         });
         editTextMorse.addTextChangedListener(new TextWatcher() {
@@ -84,46 +90,72 @@ public class MainActivity extends AppCompatActivity {
                 textViewSorti.setText(traducteurMorse.toAlpha(editable.toString()));
                 TextView textViewEntre = (TextView) findViewById(R.id.textEntre);
                 textViewEntre.setText(editTextMorse.getText());
+                alpha = traducteurMorse.toAlpha(editable.toString());
             }
         });
 
     }
 
+    /**
+     *
+     * @param v
+     */
     public void funcTire(View v) {
         final EditText editTextMorse = (EditText) findViewById(R.id.editTextMorse);
         morse += "-";
         editTextMorse.setText(morse);
     }
 
+    /**
+     *
+     * @param v
+     */
     public void funcPoint(View v) {
         final EditText editTextMorse = (EditText) findViewById(R.id.editTextMorse);
         morse += ".";
         editTextMorse.setText(morse);
     }
 
+    /**
+     *
+     * @param v
+     */
     public void funBarreOblique(View v) {
         final EditText editTextMorse = (EditText) findViewById(R.id.editTextMorse);
         morse += "/";
         editTextMorse.setText(morse);
     }
 
+    /**
+     *
+     * @param v
+     */
     public void funcEspace(View v) {
         final EditText editTextMorse = (EditText) findViewById(R.id.editTextMorse);
         morse += " ";
         editTextMorse.setText(morse);
     }
 
+    /**
+     *
+     * @param v
+     */
     public void funcEfface(View v) {
         final EditText editTextMorse = (EditText) findViewById(R.id.editTextMorse);
         final EditText editTextAlpha = (EditText) findViewById(R.id.editTextAlpha);
         morse = "";
+        alpha = "";
         editTextMorse.setText("");
         editTextAlpha.setText("");
     }
 
+    /**
+     *
+     * @param v
+     */
     public void funcBackendBy(View v) {
         final AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Attention !");
+        builder.setTitle("Backend pour/Backend By");
         builder.setMessage(traducteurMorse.getNomProgrammeurs());
         builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface arg0, int arg1) {
@@ -134,6 +166,10 @@ public class MainActivity extends AppCompatActivity {
         alert.show();
     }
 
+    /**
+     *
+     * @param v
+     */
     public void alertAttention(View v) {
         final AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Attention !");
@@ -150,6 +186,30 @@ public class MainActivity extends AppCompatActivity {
         });
         alert = builder.create();
         alert.show();
+    }
+
+    public void playMorse(View v)
+    {
+        final MediaPlayer dah = MediaPlayer.create(this, R.raw.dah);
+        final MediaPlayer dit = MediaPlayer.create(this, R.raw.dit);
+        final MediaPlayer esp = MediaPlayer.create(this, R.raw.espace);
+        dit.start();
+        System.out.println(morse.toString());
+        String[] morseToPlay = morse.split("");
+        for (int i = 0; i < morseToPlay.length; i++) {
+            if (morseToPlay[i].equals("."))
+                dit.start();
+            if (morseToPlay[i].equals("-"))
+                dah.start();
+            if (morseToPlay[i].equals(" "))
+                esp.start();
+            if (morseToPlay[i].equals("/")) {
+                esp.start();
+                esp.start();
+            }
+
+        }
+
     }
 
 }
